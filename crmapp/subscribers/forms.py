065c8c30b6fd473f81939f119c8746d402bad7e2
Michael Gichia <mqyynm@gmail.com>
2016-10-06 21:49:09 +0300

    Created subscriber form.

---
+++ crmapp/subscribers/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class SubscriberForm(UserCreationForm):
+	email = forms.CharField(required=True, widget=forms.TextInput(attrs{'class': 'form-control'}))
+	username = forms.CharField(widget=forms.TextInput(attrs{'class': 'form-control'}))
+	password1 = forms.CharField(widget=forms.TextInput(attrs{'class': 'form-control'}))
+	password2 = forms.CharField(widget=forms.TextInput(attrs{'class': 'form-control'}))
+
+# class SearchForm(forms.ModelForm):
+# 	search_query = forms.CharField(max_length=100)
--- crmapp/subscribers/views.py
+++ crmapp/subscribers/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from subscribers.forms import SeachForm
 
 # Create your views here.
+
+# def search(request):
+# 	if request.methods == 'POST':
+# 		form = SeachForm(request.POST)
+# 		if form.is_valid():
+# 			s_query = form.cleaned_data['search_query']
+# 			s_result = SomeTable.objects.filter(name=s_query)			
+# 			return render(request, 'search_form.html', {'form': form, 's_query': s_query})
+# 	else:
+# 		form = SeachForm()
+# 	return render(request, 'search_form.html', {'form': form})
