6d396664eba3370cfb13478ad6fde7e530a5d73c
Michael Gichia <mqyynm@gmail.com>
2016-10-06 22:26:14 +0300

    Created the subscriber view.

--- crmapp/subscribers/views.py
+++ crmapp/subscribers/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-from subscribers.forms import SeachForm
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+
+from subscribers.forms import SeachForm, SeachForm
 
 # Create your views here.
+def subscriber_new(request, template='subscriber/subscriber_new.html'):
+	if request.method == 'POST':
+		form = SubscriberForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			email = form.cleaned_data['email']
+			# Create the user record
+			user = User(username=username, email=email)
+			user.set_password(password)
+			user.save()
+			return HttpResponseRedirect('/success/')
+	else:
+		form = SubscriberForm()
+		return render(request, template, {'form': form})
+
 
 # def search(request):
 # 	if request.methods == 'POST':
