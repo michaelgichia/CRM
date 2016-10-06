0ffad60fccd77feef117782164d1897a9429942c
Michael Gichia <mqyynm@gmail.com>
2016-10-06 10:55:13 +0300

    Configure heroku.

--- crmapp/marketing/views.py
+++ crmapp/marketing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
 
 # Create your views here.
+class Homepage(TemplateView):
+	"""This a home page for marketing app
+	"""
+	template_name = 'marketing/home.html'
--- crmapp/urls.py
+++ crmapp/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from marketing.views import Homepage
+
 urlpatterns = patterns('',
+
+	# Market pages url.
+	
+	url(r'^$', Homepage.as_view(), name='home'),
     # Examples:
     # url(r'^$', 'crmapp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
