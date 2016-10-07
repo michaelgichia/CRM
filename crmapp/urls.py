d2fecfd071e34d025331a20cc846561caf731161
Michael Gichia <mqyynm@gmail.com>
2016-10-07 11:13:16 +0300

    Created login and logout url.

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -16,4 +16,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    # Login/Logout URLs
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/login/'}),
 )
