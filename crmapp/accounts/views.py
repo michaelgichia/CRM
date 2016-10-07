76529ab2e853bedd7af3f713054e83bc48c9cb82
Michael Gichia <mqyynm@gmail.com>
2016-10-07 15:35:22 +0300

    Created list view for accounts.

--- crmapp/accounts/views.py
+++ crmapp/accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.generic.views import ListView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+from .models import Account
+
 
 # Create your views here.
+class AccountList(ListView):
+	model = Account
+	template_name = 'accounts/account_list.html'
+	context_object_name = 'account'
+
+	def query_set(self):
+		account_list = Account.objects.filter(owner=self.request.user)
+		return account_list
+
+	@method_decorator(login_required)
+	def dispatch(self, *agrs, **kwargs):
+		super(AccountList, self).dispatch(*agrs, **kwargs)
--- crmapp/urls.py
+++ crmapp/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from marketing.views import Homepage
+from accounts.views import AccountList
 
 admin.autodiscover()
 
@@ -20,4 +21,5 @@ urlpatterns = patterns('',
     # Login/Logout URLs
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/login/'}),
+    url(r'^account/list/$', AccountList.as_view(), name='account_list'),
 )
