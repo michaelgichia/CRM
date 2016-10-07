97da82babd70fc949f510b0643aba0faab71b4f0
Michael Gichia <mqyynm@gmail.com>
2016-10-07 16:11:45 +0300

    Fixed errors.

--- crmapp/accounts/views.py
+++ crmapp/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.generic.views import ListView
+from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
@@ -11,23 +11,23 @@ class AccountList(ListView):
 	model = Account
 	paginate_by = 12
 	template_name = 'accounts/account_list.html'
-	context_object_name = 'account'
+	context_object_name = 'accounts'
 
-    def get_queryset(self):
-        try:
-            a = self.request.GET.get('account',)
-        except KeyError:
-            a = None
-        if a:
-            account_list = Account.objects.filter(
-                name__icontains=a,
-                owner=self.request.user
-            )
-        else:
-            account_list = Account.objects.filter(owner=self.request.user)
-        return account_list
+	def get_queryset(self):
+		try:
+			a = self.request.GET.get('account',)
+		except KeyError:
+			a = None
+		if a:
+			account_list = Account.objects.filter(
+			name__icontains=a,
+			owner=self.request.user
+			)
+		else:
+			account_list = Account.objects.filter(owner=self.request.user)
+		return account_list
 
 
 	@method_decorator(login_required)
 	def dispatch(self, *agrs, **kwargs):
-		super(AccountList, self).dispatch(*agrs, **kwargs)
+		return super(AccountList, self).dispatch(*agrs, **kwargs)
