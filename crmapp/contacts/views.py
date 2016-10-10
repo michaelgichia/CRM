b9fa6e9bd5315f0347f6b173cadd5f8a198ae07f
Michael Gichia <mqyynm@gmail.com>
2016-10-10 08:44:59 +0300

    integrated contact details into the account detail view

--- crmapp/contacts/views.py
+++ crmapp/contacts/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http.HttpResponseForbidden
 
 from .models import Contact
+from crmapp.accounts.models import Account
 
 # Create your views here.
 @login_required()
-def contact_detail(request, uuid):
-	contact = Contact.objects.get(uuid=uuid)
+def account_detail(request, uuid):
+	account = Account.objects.get(uuid=uuid)
 
-	return render(request, 'contacts/contact_detail.html', {'contact': contact})
+	if account.owner != request.user:
+		return HttpResponseForbidden()
+	contact = Contact.objects.filter(account=account)
+
+	variables = {
+				'account': account,
+				'contact': contact,}
+
+	return render(request, 'contacts/contact_detail.html', variables)
